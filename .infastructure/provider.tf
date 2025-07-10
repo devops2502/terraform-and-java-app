e72647cede81928672f49ac714ed9c30ba0c6092
Nguyen Manh Lam <manhlam080102@gmail.com>
2025-07-10 18:34:47 +0700

    provider.tf

--- .infastructure/provider.tf
+++ .infastructure/provider.tf
@@ -1,13 +1,13 @@
 # Terraform block
-terraform {
-  required_version = "~> 1.1"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
+# terraform {
+#   required_version = "~> 1.1"
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 6.0"
+#     }
+#   }
+# }
 
 # Provider block
 # provider "aws" {
