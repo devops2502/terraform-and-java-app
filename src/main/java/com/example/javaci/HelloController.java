70c3d8300e2c668e4b3269ac785f1800db430930
Nguyen Manh Lam <manhlam080102@gmail.com>
2025-07-10 16:41:24 +0700

    test java pl

--- src/main/java/com/example/javaci/HelloController.java
+++ src/main/java/com/example/javaci/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    // GET 
+    // GET
     @GetMapping("/")
     public String greeting() {
         return "Greetings from Spring Boot!";
